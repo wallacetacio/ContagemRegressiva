fb9e7d6f04437cf96bba1fabb4072b8a21ae301c
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-24 22:39:08 -0300

    Adiciona resolução do exercício Contagem Regressiva ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("fogo...".ToUpper());
+            Console.ResetColor();
+        }
+    }
+}
